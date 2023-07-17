55abc8a5a51e12449ca68cd5a1a2ecd80e76d300
DENNIS NGANGA <121869367+DENNIS-NGUGI@users.noreply.github.com>
2023-07-17 03:21:17 -0700

    Add files via upload

--- Fibonacci.java
+++ Fibonacci.java
@@ -2,32 +2,38 @@ package codewars.testing;
 import java.util.Scanner;
 
 public class Fibonacci {
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-	    System.out.print("Enter the number of terms: ");
-	    int numItems = scanner.nextInt();
-	    
-	    //Validates the Input data
-	    if (numItems < 0) { 
-            System.out.println("Error: Number of Items cannot be negative."); //displays Error msg
+    private static int[] memo;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of terms: ");
+        int numItems = scanner.nextInt();
+
+        // Validates the input data
+        if (numItems < 0) {
+            System.out.println("Error: Number of items cannot be negative.");
         } else {
-            System.out.println("Fibonacci Sequence up to " + numItems + " Items:");
+            System.out.println("Fibonacci Sequence up to " + numItems + " items:");
+            memo = new int[numItems + 1]; // Create an array to store Fibonacci numbers
             for (int i = 0; i < numItems; i++) {
                 System.out.print(fibonacci(i) + " ");
             }
         }
+        scanner.close();
     }
-	    
 
-    // Recursive method to calculate the Fibonacci number at position n
     public static int fibonacci(int n) {
-        // Base case: if n is 0 or 1, return n
         if (n <= 1) {
             return n;
-        } else {
-            // Recursive case: calculate Fibonacci number by summing previous two numbers
-            return fibonacci(n - 1) + fibonacci(n - 2);
         }
+
+        // Check if the Fibonacci number is already computed
+        if (memo[n] != 0) {
+            return memo[n];
+        }
+
+        // Compute the Fibonacci number and store it in the memo array
+        memo[n] = fibonacci(n - 1) + fibonacci(n - 2);
+        return memo[n];
     }
 }
-
